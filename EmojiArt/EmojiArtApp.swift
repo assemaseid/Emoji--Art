3d9543cf08b7c9f32aea505684b84a47ad8d268b
assemaseid <seidkarim601705@gmail.com>
2024-04-24 01:04:46 +0500

    Initial Commit

---
+++ EmojiArt/EmojiArtApp.swift
@@ -0,0 +1,17 @@
+//
+//  EmojiArtApp.swift
+//  EmojiArt
+//
+//  Created by Assem Seidkarim on 24.04.2024.
+//
+
+import SwiftUI
+
+@main
+struct EmojiArtApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
